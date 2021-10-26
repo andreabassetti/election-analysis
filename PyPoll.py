560591ce6db15721868f1e96b97c318d6bf1e8bd
andreabassetti <andreajr@bassetti.net>
2021-10-25 21:33:11 -0700

    saing at night

--- PyPoll.py
+++ PyPoll.py
@@ -12,14 +12,43 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a save a file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+#create a variable to define the accumulation of total votes
+total_votes = 0
+#declare a new list for the candidates
+candidate_options = []
+#declare the empty dictionary
+candidate_votes = {}
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-    #To Do: read and analyze data here 
     file_reader = csv.reader(election_data)
     
+    #read the header row
     headers = next(file_reader)
-    print(headers)
 
+    # add the total vote count. STandard python format to increment by one is number = numer + 1 OR number +=1
     for row in file_reader:
-        print(row)
+        total_votes += 1
+        #Print the candidate name from each row
+        candidate_name = row[2]
+        #add candidate name to the candidate list 
+        if candidate_name not in candidate_options:
+            #if not in the list, then add it to the list
+            candidate_options.append(candidate_name)
+            #AND begin tracking the candidates votes
+            candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count. This needs to be out of the if statement and in the for loop to keep adding incrementally
+        candidate_votes[candidate_name] += 1
+    #print the candidate list of names 
+    print(candidate_votes)
+    #iterate through the candidate list by name
+    for candidate_name in candidate_options:
+        #retieve count votes for a candidate
+        votes = candidate_votes[candidate_name]
+        #calculate the vote percentage
+        vote_percentage = float(votes)/float(total_votes)*100
+        #print the result with a full sentence
+        print(f"{candidate_name}: received {vote_percentage:,.2f}% of the vote")
+
+
+
--- Python_practice.py
+++ Python_practice.py
@@ -5,11 +5,25 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-
+#create a variable to define the accumulation of total votes
+total_votes = 0
+#declare a new list for the candidates
+candidate_options = []
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
-    headers = next(file_reader)
-    print(headers)
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+# Print the candidate list.
+print(candidate_options)
